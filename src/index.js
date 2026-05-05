51ed589af2bbed4ecce5652934019252d387d36c
oğulcan <123860363+prmdogi2@users.noreply.github.com>
2026-05-05 22:08:09 +0300

    ödev3

--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
 import { setupServer } from './server.js';
-import { initMongoConnection } from './db/models/initMongoConnection.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
 
-async function start() {
-  try {
-    console.log('Starting Mongo connection...');
-    await initMongoConnection();
-
-    console.log('Mongo connected, starting server...');
-    const app = setupServer();
-
-    const PORT = process.env.PORT || 3000;
-
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error('Error during startup:', error);
-    process.exit(1);
-  }
-}
-
-start();
+const bootstrap = async () => {
+  await initMongoConnection();
+  setupServer();
+};
+bootstrap();
--- src/server.js
+++ src/server.js
@@ -1,28 +1,39 @@
 import express from 'express';
+import { getEnvVar } from './utils/env.js';
+import pinoHttp from 'pino-http';
 import cors from 'cors';
-import pino from 'pino-http';
+import helmet from 'helmet';
+import logger from './config/logger.js';
+import { corsOptions } from './config/cors.js';
+import contactsRouter from './routers/contact.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
-import {
-  handleGetContacts,
-  handleGetContactById,
-} from './controllers/contactsController.js';
+const PORT = Number(getEnvVar('PORT', '3000'));
 
-export function setupServer() {
+export const setupServer = () => {
   const app = express();
 
-  // Middlewares
-  app.use(cors());
-  app.use(pino());
+  app.use(helmet());
+  app.use(cors({ corsOptions }));
+  app.use(pinoHttp({ logger }));
   app.use(express.json());
 
-  // Routes
-  app.get('/contacts', handleGetContacts);
-  app.get('/contacts/:contactId', handleGetContactById);
-
-  // 404 handler
-  app.use((req, res) => {
-    res.status(404).json({ message: 'Not found' });
+  app.get('/', (req, res) => {
+    res.json({
+      status: 200,
+      message: 'Welcome to the Contacts API!',
+    });
   });
 
-  return app;
-}
+  app.use(contactsRouter);
+
+  // ERROR HANDLING MIDDLEWARE
+  app.use(notFoundHandler);
+
+  app.use(errorHandler);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
